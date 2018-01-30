7e0354ded18e5e814255c9a7310692906f88c85c
Josh <jbaruch77@yahoo.com>
2018-01-30 09:34:31 -0500

    push for merge

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,13 @@
 const Router = require('koa-router');
 const router = new Router();
-const { User } = require('../db/models');
+const { User, Listing } = require('../db/models');
 
 module.exports = router;
 
 router.get('/', async (ctx) => {
   ctx.body = await User.findAll({
-    include: []
+    include: [],
+    attributes: ['email', 'firstName', 'lastName']
   });
 })
 
@@ -14,6 +15,21 @@ router.get('/:id', async (ctx) => {
   ctx.body = await User.findOne({
     where: {
       id: ctx.params.id
+    },
+    attributes: ['email', 'firstName', 'lastName']
+  })
+})
+
+router.get('/:id/listings', async (ctx) => {
+  let user = await User.findOne({
+    where: {
+      id: ctx.params.id
     }
   })
+  let userListings = await user.getListings();
+  ctx.body = userListings;
+})
+
+router.post('/', async (ctx) => {
+  ctx.body = await User.create(ctx.request.body);
 })
