537f639321c96f2c421ad43757fee3b7b298519a
Josh Baruch <jbaruch77@yahoo.com>
2018-01-31 11:51:25 -0500

    FEAT: login/logout/me auth routes working

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,49 +1,46 @@
 const Router = require('koa-router');
 const router = new Router();
+const {User} = require('../../db/models')
 
 router.post('/login', async (ctx) => {
-    // Look for user by email address
-    let user = await User.findOne({
-        where: {
-            email: ctx.request.body.email
-        }
-    })
-    // If a user is not found 
-    if (!user) {
-        ctx.body.status = 401;
-        ctx.body.message = 'User Not Found';
-    }
-    // Else if the password is incorrect
-    else if (!user.correctPassword(ctx.request.body.password)) {
-        ctx.body.message = 'Incorrect Password';
-        ctx.body.status = 401;
-    }
-    // Otherwise, log the user in
-    else ctx.login;
+  // Look for user by email address
+  let user = await User.findOne({
+      where: {
+          email: ctx.request.body.email
+      }
   })
-  
-  router.post('/signup', (req, res, next) => {
-    User.create(req.body)
-      .then(user => {
-        req.login(user, err => (err ? next(err) : res.json(user)))
-      })
-      .catch(err => {
-        if (err.name === 'SequelizeUniqueConstraintError') {
-          res.status(401).send('User already exists')
-        } else {
-          next(err)
-        }
-      })
-  })
-  
-  router.post('/logout', (req, res) => {
-    req.logout()
-    req.session.cart = {items: {}, total: 0}
-    res.redirect('/')
-  })
-  
-  router.get('/me', (req, res) => {
-    res.json(req.user)
-  })
+  // If a user is not found 
+  if (!user) {
+      ctx.status = 401;
+      ctx.body = 'User Not Found';
+  }
+  // Else if the password is incorrect
+  else if (!user.correctPassword(ctx.request.body.password)) {
+    ctx.status = 401;
+    ctx.body = 'Invalid password';
+  }
+  // Otherwise, log the user in
+  else await ctx.login(user);
+  ctx.body = 'Succesful Login'
+})
 
+router.post('/logout', (ctx) => {
+  ctx.logout();
+  ctx.body = 'logged out';
+})
+
+router.post('/signup', async (ctx) => {
+  let user = await User.findOne({
+    where: {email: ctx.request.body.email}
+  })
+  if (!user) {
+    user = await User.create(ctx.request.body);
+    ctx.body = user;
+  }
+  else ctx.body = 'User already exists';
+})
+
+router.get('/me', (ctx) => {
+  ctx.body = ctx.state.user;
+})
 module.exports = router;
