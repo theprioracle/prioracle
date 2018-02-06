b35a3ea35ae22a33989a0e7824676d659300dedf
Vinit Parkar <vinit.parkar@gmail.com>
2018-02-06 10:43:51 -0500

    Change Scrapper price to cents

--- server/scraper.js
+++ server/scraper.js
@@ -30,7 +30,7 @@ async function scrapePrice(keyword, condition) {
       avgPrice += price;
       count++;
     });
-    mean = Math.round(avgPrice/count)/100;
+    mean = Math.round(avgPrice/count);
     if(condition === "Like New")
       mean = (mean*9)/10;
     else if(condition === "Good")
@@ -41,8 +41,8 @@ async function scrapePrice(keyword, condition) {
       mean = (mean*6)/10;
 
     return {
-      min:  min/100,
-      max: max/100,
+      min:  min,
+      max: max,
       mean
     };
 
